770d50f58ead477bc63549796c6fdd0c599546ec
maulidaprina <mldaprna@gmail.com>
2024-11-08 13:43:30 +0700

    Modifikasi

--- Bioskop12.java
+++ Bioskop12.java
@@ -1,6 +1,6 @@
 public class Bioskop12 {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
     String[][] penonton = new String[4][2];
 
@@ -11,15 +11,21 @@ public class Bioskop12 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-        
-    System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
-    System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);                  
-    System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
-    System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+        penonton[3][1] = "Hana";
+
+        System.out.println(penonton.length);
+            for (String[] barisPenonton : penonton) {
+                System.out.println("Panjang Baris: " + barisPenonton.length);
+            }
+
+        System.out.println("Penonton Pada Baris Ke-3: ");
+            for (String i : penonton[2]) {
+                System.out.println((i));
+            }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton Pada Baris Ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+        }
 
     }
 }
-
-    
-    
-
